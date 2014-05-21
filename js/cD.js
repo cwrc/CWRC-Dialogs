a605e99e4a1f80c3cd76e7b2ab635eea5ab1e050
adminuser <adminuser@ubuntu.(none)>
2014-05-20 21:33:25 -0700

    fixed wrapping roleterm in role

--- js/cD.js
+++ js/cD.js
@@ -3,8 +3,8 @@
 $(function(){
 	cD = {};
 	(function(){
-		//var cwrcApi = new CwrcApi('http://apps.testing.cwrc.ca/services/ccm-api/', $);
-		 var cwrcApi = new CwrcApi('http://localhost/cwrc/', $);
+		var cwrcApi = new CwrcApi('http://apps.testing.cwrc.ca/services/ccm-api/', $);
+		 //var cwrcApi = new CwrcApi('http://localhost/cwrc/', $);
 		
 		// parameters
 
@@ -986,7 +986,7 @@ $(function(){
 					roleTerm.appendChild(entity.selfWorking.createTextNode("Author"));
 					$(role).append(roleTerm);
 				
-					$(name).append(roleTerm);
+					$(name).append(role);
 					mods.append(name);
 				}
 			});
